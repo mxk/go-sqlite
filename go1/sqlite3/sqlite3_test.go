37324b0c6378ff3a2181bbf6d253e7d7d65a7863
Maxim Khitrov <max@mxcrypt.com>
2013-06-09 09:22:40 -0400

    Use standard Go copyright notice.

--- go1/sqlite3/sqlite3_test.go
+++ go1/sqlite3/sqlite3_test.go
@@ -1,6 +1,6 @@
-//
-// Written by Maxim Khitrov (February 2013)
-//
+// Copyright 2013 The Go-SQLite Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package sqlite3_test
 
