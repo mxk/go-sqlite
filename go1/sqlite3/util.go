5c1046a7ab44b214b05c2f4b65ff28ccb11ea1e5
Maxim Khitrov <max@mxcrypt.com>
2013-09-06 13:50:04 -0400

    Remove shell.

--- go1/sqlite3/util.go
+++ go1/sqlite3/util.go
@@ -6,15 +6,12 @@ package sqlite3
 
 /*
 #include "sqlite3.h"
-
-int shell_main(int,void*);
 */
 import "C"
 
 import (
 	"bytes"
 	"fmt"
-	"os"
 	"reflect"
 	"unsafe"
 )
@@ -142,29 +139,6 @@ var (
 	ErrBadIO     = &Error{MISUSE, "closed or invalid incremental I/O operation"}
 	ErrBadBackup = &Error{MISUSE, "closed or invalid backup operation"}
 )
-
-// Shell is deprecated and will be removed in the near future. Don't use it!
-func Shell(args ...string) int {
-	if initErr != nil {
-		return 127
-	}
-	args = append([]string{os.Args[0]}, args...)
-
-	// Copy all arguments into a single []byte, terminating each one with '\0'
-	buf := make([]byte, 0, 256)
-	for _, arg := range args {
-		buf = append(append(buf, arg...), 0)
-	}
-
-	// Fill argv with pointers to the start of each null-terminated string
-	argv := make([]uintptr, len(args))
-	base := uintptr(cBytes(buf))
-	for i, arg := range args {
-		argv[i] = base
-		base += uintptr(len(arg)) + 1
-	}
-	return int(C.shell_main(C.int(len(args)), unsafe.Pointer(&argv[0])))
-}
 
 // Complete returns true if sql appears to contain a complete statement that is
 // ready to be parsed. This does not validate the statement syntax.
